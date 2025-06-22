7a6d95fb8b24aacc272b1221d8ed22e33f5c73ca
Phucduy325 <vuduyphuc674@gmail.com>
2025-06-22 21:02:32 +0700

    Phuc Duy UI

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -5,7 +5,7 @@ export default function AuthLayout() {
     <Stack>
       <Stack.Screen name="splash" options={{ headerShown: false }} />
       <Stack.Screen name="sign-in" options={{ headerShown: false }} />
-      <Stack.Screen name="sign-up" options={{ title: 'Create account' }} />
+      <Stack.Screen name="sign-up" options={{ title: 'Đăng kí' }} />
       <Stack.Screen name="email-verification" options={{ title: 'Xác thực email' }} />
       <Stack.Screen name="create-account-success" options={{ headerShown: false }} />
       <Stack.Screen name="forgot-password" options={{ title: 'Quên mật khẩu?' }} />
